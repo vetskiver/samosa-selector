c8f3502ad95a1cfe409f6c883da4c8c2432a3111
Ali Reza Ghasemi <alir.ghase@gmail.com>
2024-08-01 22:54:39 +0100

    added a counter to the cookie image

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,23 @@
 import './App.css';
+import { useState } from 'react';
 import samosaImage from './assets/samosa.png';
 
 const App = () => {
+
+  const [count, setCount] = useState(0);
+  const [multiplier, setMultiplier] = useState(1);
+
+  const updateCount = () => {
+    setCount(count + multiplier)
+  };
+
 
   return (
     <div className="App">
       <div className='header'>
         <h1>Samosa Selector</h1>
-        <h2>Count:</h2>
-        <img src={samosaImage} className="samosa" alt="Samosa" />
+        <h2>Count: {count}</h2>
+        <img src={samosaImage} className="samosa" alt="Samosa" onClick={updateCount} />
       </div>
     </div>
   )
