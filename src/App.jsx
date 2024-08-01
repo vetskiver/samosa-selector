552366a53bb09d7cef3578ad0dff4641568a3f76
Ali Reza Ghasemi <alir.ghase@gmail.com>
2024-08-01 23:10:09 +0100

    added and handled upgrades

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,24 @@ const App = () => {
     setCount(count + multiplier)
   };
 
+  const buyDoubleStuffed = () => {
+    if (count >= 10) {
+      setMultiplier(multiplier * 2);  
+    }
+  };
+
+  const buyPartyPark = () => {
+    if (count >= 100) {
+      setMultiplier(multiplier * 5);
+    }
+  };
+
+  const buyFullFeast = () => {
+    if (count >= 1000) {
+      setMultiplier(multiplier * 10)
+    }
+  };
+
   return (
     <div className="App">
       <div className='header'>
@@ -23,17 +41,17 @@ const App = () => {
         <div className='upgrade'>
           <h3>Double Stuffed 👯‍♀️</h3>
           <p>2x per click</p>
-          <button>10 samosas</button>
+          <button onClick={buyDoubleStuffed}>10 samosas</button>
         </div>
         <div className='upgrade'>
           <h3>Party Pack 🎉</h3>
           <p>5x per click</p>
-          <button>100 samosas</button>
+          <button onClick={buyPartyPark}>100 samosas</button>
         </div>
         <div className='upgrade'>
           <h3>Full Feast 👩🏽‍🍳</h3>
           <p>10x per click</p>
-          <button>1000 samosas</button>
+          <button onClick={buyFullFeast}>1000 samosas</button>
         </div>
       </div>
     </div>
