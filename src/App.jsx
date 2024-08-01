0390f7afe84263b088636df9e96a85d067a04217
Ali Reza Ghasemi <alir.ghase@gmail.com>
2024-08-01 23:03:07 +0100

    display the three possible upgrades

--- src/App.jsx
+++ src/App.jsx
@@ -11,13 +11,30 @@ const App = () => {
     setCount(count + multiplier)
   };
 
-
   return (
     <div className="App">
       <div className='header'>
         <h1>Samosa Selector</h1>
         <h2>Count: {count}</h2>
         <img src={samosaImage} className="samosa" alt="Samosa" onClick={updateCount} />
+      </div>
+
+      <div className='container'>
+        <div className='upgrade'>
+          <h3>Double Stuffed 👯‍♀️</h3>
+          <p>2x per click</p>
+          <button>10 samosas</button>
+        </div>
+        <div className='upgrade'>
+          <h3>Party Pack 🎉</h3>
+          <p>5x per click</p>
+          <button>100 samosas</button>
+        </div>
+        <div className='upgrade'>
+          <h3>Full Feast 👩🏽‍🍳</h3>
+          <p>10x per click</p>
+          <button>1000 samosas</button>
+        </div>
       </div>
     </div>
   )
