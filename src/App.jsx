2da5fe25d4815e52a4e5ba4c39f77b0b05ae57f0
Ali Reza Ghasemi <alir.ghase@gmail.com>
2024-08-01 22:07:37 +0100

    uploaded picture of samosa

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import './App.css';
+import samosaImage from './assets/samosa.png';
 
 const App = () => {
 
   return (
     <div className="App">
-
+      <div className='header'>
+        <h1>Samosa Selector</h1>
+        <h2>Count:</h2>
+        <img src={samosaImage} alt="Samosa" />
+      </div>
     </div>
   )
 }
